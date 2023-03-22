87a715ba654632078d1e6df669b34b4269da1cc1
Trung <ngoquangtrung2047@gmail.com>
2023-03-22 23:33:27 +0700

    Add validate for comment.

--- movie/src/pages/Home/Home.js
+++ movie/src/pages/Home/Home.js
@@ -1,20 +1,21 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import Header from '../../components/header'
 import Sidebar from '../../components/sidebar/index'
 import Content from '../../components/content/index'
 import './Home.scss'
 
-function Home(){
-    const [type, setType] = useState();
-    return(
-        <div>
-            <Header/>
-            <div className='container'>
-                <Sidebar  setType={setType}/>
-                <Content type={type}/>
-            </div>
-        </div>
-    )
+function Home() {
+	const [type, setType] = useState();
+	const [search, setSearch] = useState();
+	return (
+		<div>
+			<Header setSearch={setSearch} />
+			<div className='container'>
+				<Sidebar setType={setType} />
+				<Content type={type} />
+			</div>
+		</div>
+	)
 }
 
 export default Home
